c267b50d1ce52e792d4f98866b21f0321d4e7556
rene1302 <proyectos.ramp@gmail.com>
2020-12-29 17:55:20 -0300

    Nada

--- admin-api-shopify/apishopify/api.py
+++ admin-api-shopify/apishopify/api.py
@@ -12,7 +12,23 @@ load_dotenv()
 class Api:
 
     def getFile(self, file_origin):
-        print(file_origin)
+        ruta = str(pathlib.Path().absolute()) + "/admin-api-shopify/apishopify/" + file_origin
+        workbook = xlrd.open_workbook(ruta, formatting_info=True)
+        sheet = workbook.sheet_by_index(0)
+
+        for i in range(1, sheet.nrows):
+            sku =  str(repr(sheet.cell_value(i,0)))
+            nombre =  str(repr(sheet.cell_value(i,1)))
+            descripcion =  str(repr(sheet.cell_value(i,2)))
+            precio_base = int(float(repr(sheet.cell_value(i,3))))
+            precio_descuento = int(float(repr(sheet.cell_value(i,4))))
+            activo =  str(repr(sheet.cell_value(i,5)))
+            tags =  str(repr(sheet.cell_value(i,6)))
+            stock = int(float(repr(sheet.cell_value(i,7))))
+            peso = float(repr(sheet.cell_value(i,8)))
+            unidad_peso =  str(repr(sheet.cell_value(i,9)))
+            proveedor =  str(repr(sheet.cell_value(i,10)))
+            categoria =  str(repr(sheet.cell_value(i,11)))
 
     def createNewProduct(self):
 
